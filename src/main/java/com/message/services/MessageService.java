31e8daafd70ed334cf02f3eb90056e6a0325a304
Bulat <bulat@MacBook-Pro-Bulat-3.local>
2024-12-07 10:31:44 +0300

    Булат Ахметзянов: изменил MessageService.java

--- src/main/java/com/message/services/MessageService.java
+++ src/main/java/com/message/services/MessageService.java
@@ -9,4 +9,14 @@ public class MessageService {
 
 		return new Message(sender, receiver, messageText, time);
 	}
+
+	public Message[] create4Messages(User[] users)  {
+		Message[] messages = new Message[4];
+		messages[0] = send(users[0], users[1], "test");
+		messages[1] = send(users[1], users[2], "test");
+		messages[2] = send(users[2], users[3], "test");
+		messages[3] = send(users[3], users[0], "test");
+
+		return messages;
+	}
 }
