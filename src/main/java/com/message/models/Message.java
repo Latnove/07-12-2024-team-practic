da6db1e7dc4b36ac2307733b2dd18f0c9ea65415
Bulat <bulat@MacBook-Pro-Bulat-3.local>
2024-12-07 09:26:58 +0300

    author commit: Ахметзянов Булат, создана модель сообщения

---
+++ src/main/java/com/message/models/Message.java
@@ -0,0 +1,31 @@
+package com.message.models;
+
+public class Message {
+	private User sender;
+	private User reseiver;
+	private String messageText;
+	private String messageTime;
+
+  public Message(User sender, User reseiver, String messageText, String messageTime) {
+		this.sender = sender;
+		this.reseiver = reseiver;
+		this.messageText = messageText;
+		this.messageTime = messageTime;
+	}
+
+	public User getSender() {
+		return this.sender;
+	}
+
+	public User getReseiver() {
+		return  this.reseiver;
+	}
+
+	public String getMessageText() {
+		return this.messageText;
+	}
+
+	public String getMessageTime() {
+		return this.messageTime;
+	}
+}
