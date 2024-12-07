33bf2fac86fd046e668256e0fcda26ebbd1b33d5
Kemalthes <kemalmutalup@gmail.com>
2024-12-07 09:41:51 +0300

    Кемал Муталуп: bug fix

--- src/main/java/com/message/models/User.java
+++ src/main/java/com/message/models/User.java
@@ -17,31 +17,11 @@ public class User {
         return nickname;
     }
 
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getPhoneNumber() {
         return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
